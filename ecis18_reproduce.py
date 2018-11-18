adfc6ee8165af8f05c9bdd29194f427ba4750bee
Iaroslav Shcherbatyi <iaroslav.email@gmail.com>
2018-11-18 16:19:33 +0100

    Add initial ECIS efficient experiments

--- ecis18_reproduce.py
+++ ecis18_reproduce.py
@@ -10,5 +10,5 @@ dataset_path = os.path.join('datasets', 'wiki4he', 'wiki.csv')
 for model in ['gbrt']:
     for use_resp_data in [False]:
         for discount in [0.2, 0.5, 0.8, 1.0]:
-            results_path = os.path.join('experimental_results', 'nto1', 'wiki4he_%s' % discount)
-            extract_n_to_1(dataset_path, results_path, model, use_resp_data, max_iter=2, discount= discount)
+            results_path = os.path.join('experimental_results', 'nto1_2.', 'wiki4he_%s' % discount)
+            extract_n_to_1(dataset_path, results_path, model, use_resp_data, max_iter=64, discount=discount)
